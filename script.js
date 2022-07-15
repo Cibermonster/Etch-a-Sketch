61bda8d6f3d83172decb532719e2d16d92bc353e
Ciber <cibermonster@gmail.com>
2022-07-15 14:53:02 -0400

    Sizer Finished

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const btnGray = document.createElement('button');
 const btnResize = document.createElement('button');
 const btnRGB = document.createElement('button');
 const buttonsContainer = document.querySelector('.buttons');
+const sizeContainer = document.querySelector('.sizeSelection');
 
 function createDivs(col, row) {
     for (let i = 0; i < (col * row); i++) {
@@ -16,7 +17,7 @@ function createDivs(col, row) {
     container.setAttribute('id', 'sketchPad');
 }
 
-createDivs(10,10);
+createDivs(16,16);
 
 function grayColor() {
     const boxs = container.querySelectorAll('.box')
@@ -30,7 +31,6 @@ function grayColor() {
     })
     buttonsContainer.appendChild(btnGray).classList.add('btn')
 }
-
 grayColor();
 
 function reset() {
@@ -44,15 +44,17 @@ function resizeButton() {
     btnResize.addEventListener('click', () => {
         reset();
         createDivs(slider.value,slider.value);
+        grayColor();
     })
-    buttonsContainer.appendChild(btnResize).classList.add('btn')
+    sizeContainer.appendChild(btnResize).classList.add('btn')
 }
 resizeButton();
 
 var slider = document.getElementById("sizeSlider");
 var output = document.getElementById("sze");
 
-output.innerHTML = slider.value;
+slider.value = 16
+output.innerHTML = slider.value +' x '+slider.value;
 
 slider.oninput = function() {
   output.innerHTML = this.value +' x '+this.value;
