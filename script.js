55b8211f04b6739c83cc78373eac83cc9b545339
Ciber <cibermonster@gmail.com>
2022-07-15 14:34:19 -0400

    Building Resize

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 const container = document.querySelector('.container');
 const btnBlack = document.createElement('button');
 const btnGray = document.createElement('button');
+const btnResize = document.createElement('button');
 const btnRGB = document.createElement('button');
-const btnSize = document.createElement('button');
-const buttonsContainer = document.createElement('buttons');
+const buttonsContainer = document.querySelector('.buttons');
 
 function createDivs(col, row) {
     for (let i = 0; i < (col * row); i++) {
-        const div = document.createElement('div')
-        div.style.border = '1px solid red'
-        container.style.gridTemplateColumns = `repeat(${col}, ifr)`;
-        container.style.gridTemplateRows = `repeat(${row}, ifr)`;
+        const div = document.createElement('div');
+        div.style.border = '1px solid red';
+        container.style.gridTemplateColumns = `repeat(${col}, 1fr)`;
+        container.style.gridTemplateRows = `repeat(${row}, 1fr)`;
         container.appendChild(div).classList.add('box')
     }
+    container.setAttribute('id', 'sketchPad');
 }
 
-createDivs(16,16);
+createDivs(10,10);
 
 function grayColor() {
     const boxs = container.querySelectorAll('.box')
@@ -23,10 +24,35 @@ function grayColor() {
     btnGray.addEventListener('click', () => {
         boxs.forEach(box => box.addEventListener('mouseover', () => {
             let Rnum = Math.floor(Math.random() * 255)
-            box.style.background = `rgb(${Rnum},${Rnum},${Rnum},)`
+            box.style.background = `rgb(${Rnum},${Rnum},${Rnum})`
         }))
 
     })
     buttonsContainer.appendChild(btnGray).classList.add('btn')
 }
+
 grayColor();
+
+function resizeTo() {
+    
+}
+
+function resizeButton() {
+    const boxs = container.querySelectorAll('.box')
+    btnResize.textContent = 'Resize'
+    btnResize.addEventListener('click', () => {
+        let user = prompt('What size?')
+        if (user === null || user < 1) {
+            resizeTo();
+            createDivs(10,10);
+        }
+    })
+    buttonsContainer.appendChild(btnResize).classList.add('btn')
+}
+resizeButton();
+
+//container.parentNode.removeChild(container);
+//createDivs(16,16);
+
+//var m = document.getElementsByName('box')
+//m.remove();
