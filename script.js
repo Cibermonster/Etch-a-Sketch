c34e419354f6137cd5ed64ae4e8097943b48cb4b
Ciber <cibermonster@gmail.com>
2022-07-15 12:02:59 -0400

    Setup Divs

---
+++ script.js
@@ -0,0 +1,7 @@
+const container = document.querySelector('#esGrid');
+
+const content = document.createElement('div');
+content.classList.add('content');
+content.textContent = 'TEST!';
+
+container.appendChild(content);
